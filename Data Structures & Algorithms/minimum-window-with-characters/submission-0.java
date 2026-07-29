12ddc30bf4292d43d278bba339fdc36bb4577632
Anant Raj <anantrajsrivastav02@gmail.com>
2026-07-29 11:58:05 +0530

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.java
@@ -0,0 +1,48 @@
+class Solution {
+    public String minWindow(String s, String t) {
+      if (s.length() < t.length()) return "";
+
+        int[] count = new int[128];
+
+        // Store frequency of characters in t
+        for (char c : t.toCharArray()) {
+            count[c]++;
+        }
+
+        int left = 0;
+        int start = 0;
+        int minLen = Integer.MAX_VALUE;
+        int required = t.length();
+
+        for (int right = 0; right < s.length(); right++) {
+
+            // Valid character found
+            if (count[s.charAt(right)] > 0) {
+                required--;
+            }
+
+            count[s.charAt(right)]--;
+
+            // Valid window
+            while (required == 0) {
+
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left;
+                }
+
+                count[s.charAt(left)]++;
+
+                if (count[s.charAt(left)] > 0) {
+                    required++;
+                }
+
+                left++;
+            }
+        }
+
+        return minLen == Integer.MAX_VALUE
+                ? ""
+                : s.substring(start, start + minLen);   
+    }
+}
