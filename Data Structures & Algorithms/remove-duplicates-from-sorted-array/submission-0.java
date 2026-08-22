89108e22a64d92c905e51558fc1d24d45f66608f
Anant Raj <anantrajsrivastav02@gmail.com>
2026-08-22 20:14:11 +0530

    Add: remove-duplicates-from-sorted-array - submission-0

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+      if(nums.length==0)  {
+        return 0;
+      }
+      int write=0;
+
+      for(int read=1;read<nums.length;read++){
+        if(nums[write]!=nums[read])
+        {
+            write++;
+            nums[write] = nums[read];
+        }
+      }
+      return write+1;   
+    }
+}
